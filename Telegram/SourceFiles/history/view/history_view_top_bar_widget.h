5f7e2fcdba885b1b4a320df270c0650f23b1087b
People-11 <41558215+People-11@users.noreply.github.com>
2025-10-18 17:24:35 +1030

    Add no quote forward
    Press Shift to trigger
    Simplify implementation

--- Telegram/SourceFiles/history/view/history_view_top_bar_widget.h
+++ Telegram/SourceFiles/history/view/history_view_top_bar_widget.h
@@ -115,6 +115,9 @@ public:
 	[[nodiscard]] rpl::producer<> messageShotSelectionRequest() const {
 		return _messageShotSelection.events();
 	}
+	[[nodiscard]] rpl::producer<> forwardWithoutSourceSelectionRequest() const {
+		return _forwardWithoutSourceSelection.events();
+	}
 	[[nodiscard]] rpl::producer<> clearSelectionRequest() const {
 		return _clearSelection.events();
 	}
@@ -281,6 +284,7 @@ private:
 	base::Timer _onlineUpdater;
 
 	rpl::event_stream<> _forwardSelection;
+	rpl::event_stream<> _forwardWithoutSourceSelection;
 	rpl::event_stream<> _sendNowSelection;
 	rpl::event_stream<> _deleteSelection;
 	rpl::event_stream<> _messageShotSelection;
